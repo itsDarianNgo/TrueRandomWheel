80c191968b2199897b0e885c3cde4ee6f47ed2a6
Darian Ngo <darianngo@gmail.com>
2025-05-28 02:46:22 -0500

    Fixed import

--- src/features/items/CurrentItemsList.jsx
+++ src/features/items/CurrentItemsList.jsx
@@ -8,7 +8,7 @@ import {
     incrementItemGroupQuantity,
     decrementItemGroupQuantity
 } from './itemSlice';
-import { selectWheelStatus } from '../wheel/wheelSlice';
+import { selectWheelStatus } from '../wheel/WheelSlice';
 import Modal from '../../components/common/Modal';
 
 const IconMinus = ({ className = "w-4 h-4" }) => ( <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="3"><path strokeLinecap="round" strokeLinejoin="round" d="M20 12H4" /></svg>);
