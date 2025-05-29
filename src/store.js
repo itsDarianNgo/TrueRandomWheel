cadf633a20fedc824cddd878403f93d61791008c
Darian Ngo <darianngo@gmail.com>
2025-05-28 19:54:31 -0500

    Updated import

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import historyReducer from './features/history/historySlice';
 import itemReducer from './features/items/itemSlice';
-import wheelReducer from './features/wheel/wheelSlice';
+import wheelReducer from './features/wheel/WheelSlice';
 import effectsReducer from './features/effects/effectsSlice'; // New import
 
 export const store = configureStore({
